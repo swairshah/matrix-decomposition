c59d4116e8146656cb558592816bf6258a300d4a
Swair Shah <swairshah@gmail.com>
2017-06-16 13:22:33 -0500

    some fixes

--- outlier_pca.py
+++ outlier_pca.py
@@ -23,26 +23,27 @@ def outlier_pca(X, k1, k2, iters = 5):
     m, n = X.shape
     C = np.arange(n) # indices of cols of X
     C_s = np.random.choice(n, size=k1, replace=False)
-    print(C_s)
+    #print(C_s)
     C_v = np.setdiff1d(C, C_s)
 
     def run_iter(C_s, C_v):
         err_dict = {}
         V, A = VA(subtract(X, C_s), k2)
         for idx, col in enumerate(C_v):
-            err = norm(X[:, col] - V.dot(A[:,i]))
+            err = norm(X[:, col] - V.dot(A[:,idx]))
             err_dict[col] = (-1*err)
 	
 	# the following returns a list of pairs sorted 
         # by values of dict which is neg of err
 	# keys are the column indicies
+        #print(err_dict.items())
         sorted_cols = sorted(err_dict.items(), key=operator.itemgetter(1))
+        #print(sorted_cols)
     
         # now we get first k1 col indices outof it
         C_s = [i[0] for i in sorted_cols[0:k1]]
 
         C_v = np.setdiff1d(C, C_s)
-        print(sorted_cols)
         return C_s, C_v
 
     for i in range(iters):
@@ -51,21 +52,26 @@ def outlier_pca(X, k1, k2, iters = 5):
         X_ = subtract(X, C_s)
         R = reduce_ksvd(X_, k2)
         error = norm(R)
-        print(error)
+        #print(error)
 
+    print(error)
     return C_s
 
 def avg_outlier_pca(X, k1, k2, iters = 5):
+    np.set_printoptions(suppress = True, precision = True)
     """
     C_s contains col indices included in S
     C_v contains rest 
+    we maintain a running avg of error x_i - V*a_i 
+    for each column (in C_v).
     """
     m, n = X.shape
     C = np.arange(n) # indices of cols of X
 
-    score_dict = {i: 0 for i in C}
+    score_dict = {i: [0] for i in C}
+    avg_score_dict = {i: np.mean(score_dict[i]) for i in score_dict}
     C_s = np.random.choice(n, size=k1, replace=False)
-    print(C_s)
+    #print(C_s)
     C_v = np.setdiff1d(C, C_s)
 
     iter = 1.0
@@ -73,22 +79,18 @@ def avg_outlier_pca(X, k1, k2, iters = 5):
         err_dict = {}
         V, A = VA(subtract(X, C_s), k2)
         for idx, col in enumerate(C_v):
-            err = norm(X[:, col] - V.dot(A[:,i]))
-            score_dict[col] += err/iter
-
-        for col in C:
-            score_dict[col] *= iter/(iter+1)
+            err = norm(X[:, col] - V.dot(A[:,idx]))
+            score_dict[col].append(err)
 	
+        avg_score_dict = {i: np.mean(score_dict[i]) for i in score_dict}
 	# the following returns a list of pairs sorted 
         # by values of dict which is running avg or err
 	# keys are the column indicies
-        sorted_cols = sorted(score_dict.items(), key=operator.itemgetter(1))
-    
+        #print(avg_score_dict)
+        sorted_cols = sorted(avg_score_dict.items(), key=operator.itemgetter(1))
         # now we get last k1 col indices outof it
         C_s = [i[0] for i in sorted_cols[-k1:]]
-
         C_v = np.setdiff1d(C, C_s)
-        print(sorted_cols)
         return C_s, C_v
 
     for i in range(iters):
@@ -98,12 +100,14 @@ def avg_outlier_pca(X, k1, k2, iters = 5):
         R = reduce_ksvd(X_, k2)
         error = norm(R)
         iter += 1
-        print(error)
+        #print(error)
 
+    print(error)
     return C_s
 
 if __name__ == "__main__":
-    X = np.random.uniform(size = (6,20), low = 0, high = 100)
-    outlier_pca(X, 2, 3, iters = 5)
+    np.set_printoptions(suppress = True, precision = True)
+    X = np.random.uniform(size = (10,20), low = 0, high = 100)
+    print(outlier_pca(X, 5, 4, iters = 30))
     print()
-    avg_outlier_pca(X, 2, 3, iters = 5)
+    print(avg_outlier_pca(X, 5, 4, iters = 500))
